d517c82acfdb2b6f72c286b2c324c917b61bd556
SaraKhandaker <kttsara@gmail.com>
2020-08-10 14:19:49 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data, given_season)
+  name=""
+  data.each{|season, season_hash|
+    season_hash.each{ |cont_hash|
+      name=cont_hash["name"] if cont_hash["status"]=="Winner"
+    } if season==given_season
+  }
+  name.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name=""
+  data.each{|season, season_hash|
+    season_hash.each{ |cont_hash|
+      name=cont_hash["name"] if cont_hash["occupation"]==occupation
+    }
+  }
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter=0
+  data.each{|season, season_hash|
+    season_hash.each{ |cont_hash|
+      counter+=1 if cont_hash["hometown"]==hometown
+    }
+  }
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each{|season, season_hash|
+    season_hash.each{ |cont_hash|
+     return cont_hash["occupation"] if cont_hash["hometown"]==hometown
+    }
+  }
 end
 
-def get_average_age_for_season(data, season)
-  # code here
+def get_average_age_for_season(data, given_season)
+  age=0
+  count=0
+  data.each{|season, season_hash|
+    season_hash.each{ |cont_hash|
+      age+=cont_hash["age"].to_f
+      count +=1
+    } if season==given_season
+  }
+  (age/count).round(0)
 end
